3573aa9aa9aff163800ecfd999bc4b5562e8f5d5
Hugo Devillers <hugo@xol.io>
2025-05-29 22:11:33 +0200

    working with barycentrics

--- examples/15_compute_cubes/15_compute_cubes.glsl
+++ examples/15_compute_cubes/15_compute_cubes.glsl
@@ -2,6 +2,11 @@
 #extension GL_EXT_shader_image_load_formatted : require
 #extension GL_EXT_scalar_block_layout : require
 
+precise float;
+precise vec2;
+precise vec3;
+precise vec4;
+
 layout(set = 0, binding = 0)
 uniform image2D renderTarget;
 
@@ -20,21 +25,18 @@ float cross_2(vec2 a, vec2 b) {
 }
 
 float barCoord(vec2 a, vec2 b, vec2 point){
-    vec2 PA = point - a;
-    vec2 BA = b - a;
+    precise vec2 PA = point - a;
+    precise vec2 BA = b - a;
     return cross_2(PA, BA);
 }
 
 vec3 barycentricTri2(vec2 v0, vec2 v1, vec2 v2, vec2 point) {
-    float scaling = barCoord(v0.xy, v1.xy, v2.xy);
+    precise float scale = barCoord(v0.xy, v1.xy, v2.xy);
 
-    float u = barCoord(v0.xy, v1.xy, point) / scaling;
-    float v = barCoord(v1.xy, v2.xy, point) / scaling;
+    precise float u = barCoord(v0.xy, v1.xy, point) / scale;
+    precise float v = barCoord(v1.xy, v2.xy, point) / scale;
 
-    //if (scaling > 0)
-    //    return vec3(-1);
-
-    return vec3(u, v, scaling);
+    return vec3(u, v, scale);
 }
 
 bool is_inside_edge(vec2 e0, vec2 e1, vec2 p) {
@@ -54,33 +56,34 @@ void main() {
     vec2 point = vec2(gl_GlobalInvocationID.xy) / vec2(img_size);
     point = point * 2.0 - vec2(1.0);
 
+    // original, object-space coordinates of the triangle vertices
     vec4 os_v0 = vec4(push_constants.triangle.v0, 1);
     vec4 os_v1 = vec4(push_constants.triangle.v1, 1);
     vec4 os_v2 = vec4(push_constants.triangle.v2, 1);
-    vec4 v0 = push_constants.m * os_v0;
-    vec4 v1 = push_constants.m * os_v1;
-    vec4 v2 = push_constants.m * os_v2;
-    vec2 ss_v0 = v0.xy /= v0.w;
-    vec2 ss_v1 = v1.xy /= v1.w;
-    vec2 ss_v2 = v2.xy /= v2.w;
+    // transformed triangle vertices in clip space
+    precise vec4 v0 = push_constants.m * os_v0;
+    precise vec4 v1 = push_constants.m * os_v1;
+    precise vec4 v2 = push_constants.m * os_v2;
+    // screen-space coordinates of the triangles, plus normalized Z
+    precise vec3 ss_v0 = v0.xyz /= v0.w;
+    precise vec3 ss_v1 = v1.xyz /= v1.w;
+    precise vec3 ss_v2 = v2.xyz /= v2.w;
 
-    vec3 baryResults = barycentricTri2(ss_v0.xy, ss_v1.xy, ss_v2.xy, point);
-    float u = baryResults.x;
-    float v = baryResults.y;
-    float w = 1 - u - v;
+    precise vec3 baryResults = barycentricTri2(ss_v0.xy, ss_v1.xy, ss_v2.xy, point);
+    precise float u = baryResults.x;
+    precise float v = baryResults.y;
+    precise float w = 1.0 - u - v;
+    precise float scale = baryResults.z;
 
-    //if (!(u >= 0.0 && v >= 0.0 && w >= 0.0))
+    // for the purposes of visibility, we have to invert the test whenever a given vertex lies behind the camera
+    if ((u < 0.0 ^^ v2.w < 0) || (v < 0.0 ^^ v0.w < 0) || (w < 0.0 ^^ v1.w < 0))
+        return;
+
+    // we need to invert the front/backface culling once for each vertex behind the camera
+    //if (scale > 0 ^^ v0.w < 0 ^^ v1.w < 0 ^^ v2.w < 0)
     //    return;
 
     vec4 c = vec4(push_constants.triangle.color, 1);
-
-    bool backface = ((is_inside_edge(ss_v1.xy, ss_v0.xy, point) ^^ (v0.w < 0) ^^ (v1.w < 0)) && (is_inside_edge(ss_v2.xy, ss_v1.xy, point) ^^ (v1.w < 0) ^^ (v2.w < 0)) && (is_inside_edge(ss_v0.xy, ss_v2.xy, point) ^^ (v2.w < 0) ^^ (v0.w < 0)));
-    //if (not_backface)
-    //    return;
-
-    bool frontface = (is_inside_edge(ss_v0.xy, ss_v1.xy, point) ^^ (v0.w < 0) ^^ (v1.w < 0)) && (is_inside_edge(ss_v1.xy, ss_v2.xy, point) ^^ (v1.w < 0) ^^ (v2.w < 0)) && (is_inside_edge(ss_v2.xy, ss_v0.xy, point) ^^ (v2.w < 0) ^^ (v0.w < 0));
-    if (!frontface && !backface)
-        return;
 
     vec3 v_ws = vec3(v0.w, v1.w, v2.w);
     vec3 ss_v_coefs = vec3(v, w, u);
@@ -88,7 +91,9 @@ void main() {
     float pc_v_coefs_sum = pc_v_coefs.x + pc_v_coefs.y + pc_v_coefs.z;
     pc_v_coefs = pc_v_coefs / pc_v_coefs_sum;
 
+    //float depth = dot(pc_v_coefs, vec3(v0.z, v1.z, v2.z));
     float depth = dot(pc_v_coefs, vec3(v0.w, v1.w, v2.w));
+    //float depth = dot(pc_v_coefs, vec3(ss_v0.z, ss_v1.z, ss_v2.z));
 
     float tcx = dot(vec3(os_v0.x, os_v1.x, os_v2.x), pc_v_coefs);
     float tcy = dot(vec3(os_v0.y, os_v1.y, os_v2.y), pc_v_coefs);
@@ -107,7 +112,9 @@ void main() {
     //c.xyz = vec3(depth * 0.6 + 0.2, 0.25, 0.25);
     //c.xyz = vec3(tcx, tcy, tcz);
 
+    c = c * 0.5;
     vec4 p = imageLoad(renderTarget, ivec2(gl_GlobalInvocationID.xy));
-    c.rgb = mix(c.rgb, p.rgb, 0.5);
+    c.rgb = c.rgb + p.rgb;
+    //c.rgb = mix(c.rgb, p.rgb, 0.5);
     imageStore(renderTarget, ivec2(gl_GlobalInvocationID.xy), c);
 }
